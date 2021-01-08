0a2c5ce9365056ef02c6404c72813a24dd3fe677
Alan <alanmak95@gmail.com>
2021-01-08 23:57:40 +0000

    Added sample data and code using google charts

--- server.js
+++ server.js
@@ -47,7 +47,11 @@ app.use("/api/widgets", widgetsRoutes(db));
 // Warning: avoid creating more routes in this file!
 // Separate them into separate routes files (see above).
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("results");
+});
+
+app.get("/:survey_id/results", (req, res) => {
+  res.render("results");
 });
 
 app.listen(PORT, () => {
