3f4e0e72a5b11227bf8d2e72e6a49f27eadd3d96
Alan <alanmak95@gmail.com>
2021-01-09 01:30:44 +0000

    Footer separated into own file

---
+++ db/seeds/02_polls.sql
@@ -0,0 +1,3 @@
+INSERT INTO polls (admin_id, title, user_email) VALUES (1, 'What Movie Should we See?', 'alice@alicemail.com');
+INSERT INTO polls (admin_id, title, user_email) VALUES (2, 'Where Should We Have Dinner?', 'kira@kiramail.com');
+INSERT INTO polls (admin_id, title, user_email) VALUES (1, 'Who carves the best pumpkin?', 'alice@alicemail.com');
