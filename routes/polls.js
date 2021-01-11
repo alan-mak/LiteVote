b25a5f6be93bf8765b613105bdd097ea3a00fc76
Alan <alanmak95@gmail.com>
2021-01-11 21:29:38 +0000

    Added drop down button with the amount of choices available

--- routes/polls.js
+++ routes/polls.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const router  = express.Router();
 
-const mailgun = require("mailgun-js")
+// const mailgun = require("mailgun-js")
 
 
 //Sample Email for testing
