1bc9ebe2d8c34d60715829064ee93284692deb1a
Alan <alanmak95@gmail.com>
2021-01-13 19:52:39 +0000

    Added a delete button

--- public/scripts/newOptions.js
+++ public/scripts/newOptions.js
@@ -1,13 +1,16 @@
 $(document).ready(function (){
   let text =
     `<div class=option_and_description>
-        <textarea name="option" placeholder="What is your option for this poll?"></textarea>
-        <textarea name="option_description" placeholder="(Optional) Provide a description of the option you  added."></textarea>
-    </div>
-    `;
+      <textarea name="option" placeholder="What is your option for this poll?"></textarea>
+      <textarea name="option_description" placeholder="(Optional) Provide a description of the option you  added."></textarea>
+      <button type="button" class="delete btn btn-primary"><i class="fas fa-times-circle"></i></button>
+    </div>`;
 
   let $add = $('.add');
   $add.click(function (){
     $('#option_container').append(text);
+    $('.delete').click(function() {
+      $('#option_container').remove()
+    })
   })
 });
