609b08aed52b23d64e322c21e353ceb66c4b18ba
Alan <alanmak95@gmail.com>
2021-01-13 22:51:51 +0000

    Fixed bug in error message where they would not show

--- public/scripts/errorMessage.js
+++ public/scripts/errorMessage.js
@@ -5,15 +5,19 @@ $(document).ready(function() {
     let data = $('form').serializeArray();
     let dataArr = [];
     for (let object of data) {
-      dataArr.push(object.value)
+      dataArr.push(object.value);
     }
     // Indicates a duplicate value has been selected
+    $error_duplicate = $('.error_duplicate');
+    $error_missing = $('.error_missing')
+    $error_duplicate.css('display', 'none');
+    $error_missing.css('display', 'none');
     if (new Set(dataArr).size !== dataArr.length) {
-      $('.error_duplicate').css('display', 'flex')
+      $error_duplicate.css('display', 'flex');
       event.preventDefault();
     }
     if (dataArr.includes("")) {
-      $('.error_missing').css('display', 'flex')
+      $error_missing.css('display', 'flex');
       event.preventDefault();
     }
   })
