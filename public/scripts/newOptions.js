8d6271314cee9ebca4a38f733c613209b26f6a4d
Alan <alanmak95@gmail.com>
2021-01-13 20:34:34 +0000

    Styling delete button

--- public/scripts/newOptions.js
+++ public/scripts/newOptions.js
@@ -1,15 +1,17 @@
-$(document).ready(function (){
+$(document).ready(function () {
   let text =
     `<div class=option_and_description>
-      <textarea name="option" placeholder="What is your option for this poll?"></textarea>
-      <textarea name="option_description" placeholder="(Optional) Provide a description of the option you  added."></textarea>
-      <button type="button" class="delete btn btn-primary"><i class="fas fa-times-circle"></i></button>
+      <div>
+        <textarea name="option" placeholder="What is your option for this poll?"></textarea>
+        <textarea name="option_description" placeholder="(Optional) Provide a description of the option you  added."></textarea>
+      </div>
+        <button type="button" class="delete btn btn-primary"><i class="fas fa-times-circle"></i></button>
     </div>`;
 
   let $add = $('.add');
-  $add.click(function (){
+  $add.click(function () {
     $('#option_container').append(text);
-    $('.delete').click(function() {
+    $('.delete').click(function () {
       $('#option_container').remove()
     })
   })
