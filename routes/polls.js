c20a38ffc4e32df43951ebfbf1f4a787fe94cce7
CD-Mackay <71985737+CD-Mackay@users.noreply.github.com>
2021-01-13 17:16:23 -0500

    Update polls.js

--- routes/polls.js
+++ routes/polls.js
@@ -2,9 +2,7 @@ const express = require('express');
 const { ClientBase } = require('pg');
 const router  = express.Router();
 const mailgun = require("mailgun-js");
-const API_key = "key-97d02eadbc220b398a788f87d8a288ac";
-const DOMAIN = "sandbox12380c005e9e430a9a24bf7178babe5b.mailgun.org";
-const mg = mailgun({apiKey: API_key, domain: DOMAIN});
+
 
 
 
